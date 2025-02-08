d3460ef5f2749b18f308acb0246890cf5d75634e
Shop4me.lk <90832527+Mafaz5128@users.noreply.github.com>
2025-02-08 21:04:06 +0530

    Update spin.py

--- spin.py
+++ spin.py
@@ -321,5 +321,11 @@ html_code = """
 </body>
 </html>
 """
-# Embed HTML in Streamlit
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 st.components.v1.html(html_code, height=1000, scrolling=True)
